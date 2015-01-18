a879c1f16cdd80717def4a88cc5628ffbbc904a9
CrisAdam <CristianRAdam+GitHub@gmail.com>
2015-01-17 20:31:14 -0500

    added password salt +hash

--- Server/src/ice_breaker_server/adam4/com/DatabaseRequestHandler.java
+++ Server/src/ice_breaker_server/adam4/com/DatabaseRequestHandler.java
@@ -1,44 +1,77 @@
 package ice_breaker_server.adam4.com;
 
+import java.security.SecureRandom;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class DatabaseRequestHandler
 {
-    public static boolean userExists(String userName)
-    {
-        try
-        {
-            Connection con = DatabaseHandler.getConnection();
-            java.sql.Statement existsQuery;
-            existsQuery = con.createStatement();
 
-            ResultSet existsResult = existsQuery.executeQuery("SELECT COUNT(*) from icebreaker.user_table where username=\"" + userName + "\";");
-            return (existsResult.getInt(1) == 1);
+	public static boolean userExists(String userName)
+	{
+		try
+		{
+			Connection con = DatabaseHandler.getConnection();
+			java.sql.Statement existsQuery;
+			existsQuery = con.createStatement();
 
-        }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            return false;
-        }
-    }
+			ResultSet existsResult = existsQuery.executeQuery("SELECT COUNT(*) from icebreaker.user_table where username=\"" + userName + "\";");
+			return (existsResult.getInt(1) == 1);
+
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
 
 	public static boolean connect(String username, String password)
 	{
 		if (userExists(username))
 		{
-			// todo: check that password is valid
 			return true;
 		}
 		return true;
-		
+
 	}
 
 	public static boolean join(String username, String password)
 	{
-		return !userExists(username);
-		
+		if (userExists(username))
+		{
+			return false;
+		}
+		/*
+		 * INSERT INTO `icebreaker`.`user_table` (`iduser`, `username`, `email`, `salt`, `hashedpw`, `lastlogin`) VALUES (<{iduser: }>, <{username: }>, <{email: }>, <{salt: }>, <{hashedpw: }>,
+		 * <{lastlogin: }>);
+		 */
+		PreparedStatement pstmt = null;
+		try
+		{
+			Connection con = DatabaseHandler.getConnection();
+			String query = "INSERT INTO `icebreaker`.`user_table` (`username`, `salt`, `hashedpw`) VALUES(?, ?, ?)";
+
+			final Random r = new SecureRandom();
+			byte[] salt = new byte[32];
+			r.nextBytes(salt);
+			
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, username);
+			pstmt.setString(2, r.toString());
+			pstmt.setString(3, Common.hashPassword(password, r.toString()));
+			pstmt.executeUpdate();
+			pstmt.close();
+			return true;
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+
 	}
 }
